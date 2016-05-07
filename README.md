e41a1a4386a1d3c201a0d922fdc2c3571069b7ce
Chaitanya Raju <yourchaitu@gmail.com>
2016-05-07 10:49:31 +0000

    Jade For the first time

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ImageSearch
+## ImageSearch
+
+#FreeCodeCamp API: Image Search Abstraction Layer
+
 For FCC
+
+I am using https://www.npmjs.com/package/bing.search here 
+and i am trying Jade for the first time as i have repeated the same html for the last few times.. as this api needs only few line of html i think its a good way to learn jade
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 
+var pug = require('pug');
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    var html = pug.renderFile('views/index.jade',{Url: process.env.IP});
+  res.send(html);
+});
+app.get('*', function (req, res) {
+    var html = pug.renderFile('views/error.jade',{Url:  process.env.IP});
+  res.send(html);
 });
 
 app.listen(process.env.PORT||8080,process.env.IP, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Your App is running on port '+process.env.PORT+' !! !! ' );
 });
